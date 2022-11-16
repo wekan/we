3065281475a052f9c661b4b225e70d7960a821b3
Lauri Ojansivu <x@xet7.org>
2022-11-16 14:52:38 +0200

    Some MongoDB query.

--- server/main.js
+++ server/main.js
@@ -4,12 +4,44 @@ import { onPageLoad } from "meteor/server-render";
 Meteor.startup(() => {
   // Code to run on server startup.
   console.log(`Greetings from ${module.id}!`);
+
+  // Replace the uri string with your MongoDB deployment's connection string.
+
+  const mongo = require('mongodb');
+
+  const MongoClient = mongo.MongoClient;
+
+  const url = 'mongodb://127.0.0.1:4001';
+
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+
+    if (err) throw err;
+
+    //console.log(client.topology.clientInfo);
+
+    const db = client.db("meteor");
+
+    db.listCollections().toArray().then((docs) => {
+
+        console.log('Available collections:');
+        console.log(docs.forEach((doc, idx, array) => { console.log(doc.name) }));
+
+    }).catch((err) => {
+
+        console.log(err);
+    }).finally(() => {
+
+        client.close();
+    });
+
+  });
+
 });
 
 onPageLoad(sink => {
   // Code to run on every request.
   sink.renderIntoElementById(
     "server-render-target",
-    `Server time: ${new Date}`
+    `Server time: ${new Date}`,
   );
 });
