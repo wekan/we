47d225659a574a1be2f7b5a2f7411ccc0501226c
Lauri Ojansivu <x@xet7.org>
2023-12-21 10:17:46 +0200

    Removed extra code.

--- server/main.js
+++ server/main.js
@@ -4,40 +4,6 @@ import { onPageLoad } from "meteor/server-render";
 Meteor.startup(() => {
   // Code to run on server startup.
   console.log(`Greetings from ${module.id}!`);
-
-/* No MongoDB here, commenting out.
-
-  // Replace the uri string with your MongoDB deployment's connection string.
-  const mongo = require('mongodb');
-
-  const MongoClient = mongo.MongoClient;
-
-  const url = 'mongodb://127.0.0.1:4001';
-
-  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-
-    if (err) throw err;
-
-    //console.log(client.topology.clientInfo);
-
-    const db = client.db("meteor");
-
-    db.listCollections().toArray().then((docs) => {
-
-        console.log('Available collections:');
-        console.log(docs.forEach((doc, idx, array) => { console.log(doc.name) }));
-
-    }).catch((err) => {
-
-        console.log(err);
-    }).finally(() => {
-
-        client.close();
-    });
-
-  });
-*/
-
 });
 
 onPageLoad(sink => {
